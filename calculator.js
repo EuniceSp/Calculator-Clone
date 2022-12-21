b27da181f268315b28f231c2b2a0d6041d80e2f2
EuniceSp <eunice.sp97@gmail.com>
2022-12-21 12:37:24 -0600

    Eventos definidos

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+/**Constantes de la calculadora */
 const output = document.querySelector('#output')
 const btnAc = document.querySelector('#ac')
 const btnDel = document.querySelector('#del')
@@ -17,3 +18,82 @@ const btnN7 = document.querySelector('#n7')
 const btnN8 = document.querySelector('#n8')
 const btnN9 = document.querySelector('#n9')
 const btnN0 = document.querySelector('#n0')
+
+/**Constantes para resultados */
+let op1
+let op2
+let operacion
+
+/**Eventos botones */
+btnN1.addEventListener('click', function(e){
+    output.textContent= output.textContent + "1"
+})
+
+btnN2.addEventListener('click', function(e){
+    output.textContent= output.textContent + "2"
+})
+
+btnN3.addEventListener('click', function(e){
+    output.textContent= output.textContent + "3"
+})
+
+btnN4.addEventListener('click', function(e){
+    output.textContent= output.textContent + "4"
+})
+
+btnN5.addEventListener('click', function(e){
+    output.textContent= output.textContent + "5"
+})
+
+btnN6.addEventListener('click', function(e){
+    output.textContent= output.textContent + "6"
+})
+
+btnN7.addEventListener('click', function(e){
+    output.textContent= output.textContent + "7"
+})
+
+btnN8.addEventListener('click', function(e){
+    output.textContent= output.textContent + "8"
+})
+
+btnN9.addEventListener('click', function(e){
+    output.textContent= output.textContent + "9"
+})
+
+btnN0.addEventListener('click', function(e){
+    output.textContent= output.textContent + "0"
+})
+
+btnAc.addEventListener('click', resetear())
+
+btnDel.addEventListener('click', borrar())
+
+btnMas.addEventListener('click', function(e){
+    op1 = output.textContent
+    operacion = "+"
+    limpiar()
+})
+
+btnMen.addEventListener('click', function(e){
+    op1 = output.textContent
+    operacion = "-"
+    limpiar()
+})
+
+btnPor.addEventListener('click', function(e){
+    op1 = output.textContent
+    operacion = "*"
+    limpiar()
+})
+
+btnEnt.addEventListener('click', function(e){
+    op1 = output.textContent
+    operacion = "/"
+    limpiar()
+})
+
+btnIgu.addEventListener('click', function(e){
+    op2 = output.textContent;
+    solucion()
+})
